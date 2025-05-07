7b4404377082977d75aaa419aa6c3e1f14e548f1
Almasi <almasi89>
2025-05-07 13:14:00 +0300

    adds comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,11 @@
 
 def return_evens(num_list):
     pass
+    return[num for num in num_list if num % 2==0]
 
 def make_exclamation(sentence_list):
     pass
+    if not sentence_list:
+        return[]
+    return[sentense + "!" for sentense in sentence_list]
+    
